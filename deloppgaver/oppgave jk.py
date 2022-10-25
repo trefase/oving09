c7938434730f49e857ed197265ee9a56024baec3
askeli <christoffer.askeli@gmail.com>
2022-10-25 21:04:33 +0200

    endret variabelnavn i deloppgave

--- deloppgaver/oppgave jk.py
+++ deloppgaver/oppgave jk.py
@@ -6,11 +6,11 @@ Christoffer 24.10.22
 import pandas as pd
 
 
-df = pd.read_csv('datafil.csv', sep=';', usecols=['tittel','sted','tidspunkt','varighet'])
+datafil = pd.read_csv('datafil.csv', sep=';', usecols=['tittel','sted','tidspunkt','varighet'])
 
-def datoFilter(avtaleListe, dato):
+def dato_filter(avtale_liste, dato):
    #return print(avtaleListe[avtaleListe['tidspunkt'] == dato])
-   return print(avtaleListe[avtaleListe['tidspunkt'].str.contains(dato)])
+   return print(avtale_liste[avtale_liste['tidspunkt'].str.contains(dato)])
  
-datoFilter(avtaleListe=df,dato='2023')
+dato_filter(avtale_liste=datafil,dato='2023')
 
