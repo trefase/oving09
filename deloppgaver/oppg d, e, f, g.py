d45a66c2cb326e49432374305ba04fc7a5e0560b
henryeikeland <101245341+henryeikeland@users.noreply.github.com>
2022-10-29 13:18:19 +0200

    Update oppg d, e, f, g.py

--- deloppgaver/oppg d, e, f, g.py
+++ deloppgaver/oppg d, e, f, g.py
@@ -6,14 +6,14 @@ Created on Wed Oct 26 18:49:19 2022
 """
 
 from datetime import datetime
-import numpy as np
 import pandas as pd
 
 
+dict_liste = dict()  
+liste=[]  
 
-#oppgave d)
-#klasse for avtale 
 
+#Klasse for ny avtale 
 class Avtale():
     def __init__(self, init_tittel = "", init_sted = "", init_starttidspunkt = datetime.now(), init_varighet = 0, init_kategori ="" ):
         self.tittel = init_tittel
@@ -23,19 +23,16 @@ class Avtale():
         self.kategori = init_kategori
         
 
-#oppgave e)
-#__str__
-
+#Avtale streng __str__
     def __str__(self):
         return f"{self.tittel}, {self.sted}, {self.starttidspunkt}, {self.varighet} min, {self.kategori}"
  
 
-#oppgave f)
-#funksjon ny avtale
 
-liste=[]
-dict_liste = dict()  
 
+
+
+#funksjon ny avtale - avtalen havner i liste og dictionary som objekt
 def ny_avtale():
     bekreftet = "" 
     while bekreftet != "Ja":
@@ -43,6 +40,7 @@ def ny_avtale():
         sted = input("Oppgi sted:")
         print("Oppgi tidpunkt(ÅÅÅÅ-MM-DD TT:MM:SS):")
         starttidspunkt = ""
+        
         while starttidspunkt == "":
             try:
                 starttidspunkt = datetime(int(input("ÅÅÅÅ:")),int(input("MM:")),int(input("DD:")),int(input("TT:")),int(input("MM:")))                
@@ -65,8 +63,8 @@ def ny_avtale():
        
         
         print("Bekreft ", Avtale(tittel,sted, starttidspunkt, varighet, kategori))
-        bekreftet = input("Ja/Nei:")     
-        if bekreftet == "Ja" or bekreftet =="ja": 
+        bekreftet = input("Ja/Nei:").casefold()        
+        if "ja" in bekreftet:
            
             dict_liste[tittel]=Avtale(tittel,sted, starttidspunkt, varighet, kategori)
             liste.append(Avtale(tittel,sted, starttidspunkt, varighet, kategori))
@@ -76,30 +74,30 @@ def ny_avtale():
         else:
             print("Skriv avtalen på nytt.")
             continue
-    
+
+
+
    
-#test=ny_avtale()     
-   
-  
-#oppgave g)
-#funksjon skriver ut liste med avtale
+#Valg av funksjoner som skriver ut liste med avtale, dataframe er mest oversiktlig
 
-
-#liste
-def skriv_ut(x):
+#Skriver ut liste - indeks og avtalestreng 
+def skriv_ut_liste(liste):
     print("Utskrift liste")
-    for i in range(len(x)):
-        print(i,x[i].tittel," - ",x[i])
-        #print(i,x[i].tittel,x[i].kategori)
+    for i in range(len(liste)):
+        print(i,liste[i].tittel," - ",liste[i].__str__())
 
-#skriv_ut(liste)
-
-
-#dict
-def skriv_ut_d(x):
+#Skriver ut dictionary - indeks og avtalestreng 
+def skriv_ut_dict(dict_):
     print("Utskrift liste")
-    for i in (x):
-        print(i,x[i])
-        #print(i,x[i].varighet)
-        
-#skriv_ut_d(dict_liste)
+    for i in (dict_):
+        print(i," - ",dict_[i])
+
+#Generere dataframe fra liste og skriver denne ut 
+def skriv_ut_alle_avtaler():
+    df_liste = pd.DataFrame(columns = ['tittel','sted','starttidspunkt','varighet','kategori'])
+    for i in liste:
+        df = pd.DataFrame([[i.tittel,i.sted,str(i.starttidspunkt),i.varighet,i.kategori]], columns=('tittel','sted','starttidspunkt','varighet','kategori'))
+        df_liste = df_liste.append([df], ignore_index=True)
+       
+    print("Utskrift liste")
+    print(df_liste)
